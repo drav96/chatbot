f5aa9e590b92e1ec92972b3d8eee563ebf3786d3
Dragos Lupei <dragos.lupei@sens.media>
2017-12-16 18:28:26 +0200

    add example json

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+let mcib = require('../micb_fixture.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.render('index', { title: 's' });
+});
+
+
+router.post('/', function (req, res, next) {
+	console.log(req.body)
+	res.status(200).json(mcib)
 });
 
 module.exports = router;
